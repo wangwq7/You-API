899e066635d6ead64948774ff16ce3b693cffa4f
wangwq7 <7777@linux.do>
2025-04-10 19:17:13 +0800

    Update start.go

--- start.go
+++ start.go
@@ -12,7 +12,7 @@ func main() {
 	// 将 /api/main.go 的 Handler 函数注册到根路径
 	http.HandleFunc("/", api.Handler)
 
-	port := ":8080"
+	port := ":8082"
 	fmt.Printf("Server is running on http://localhost%s\n", port)
 
 	// 启动服务器
